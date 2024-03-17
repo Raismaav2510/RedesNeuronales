afb738eea8906f056cc910f6deb8835dfbce6a33
Rafael Magaña <raismaav.2510@gmail.com>
2024-03-17 13:10:16 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import NeuralNetwork
+
+# Entrenamiento de la red neuronal
+# NeuralNetwork.training(model_name='model_extra.pth')
+# NeuralNetwork.print_plots()
+# NeuralNetwork.predict()
+
+# Evaluar datos nuevos
+print(NeuralNetwork.eval_data([8, 30.5, 0], rounded=True))
+
